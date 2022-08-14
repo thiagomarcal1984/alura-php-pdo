1054e2f8fc715ddd08350eb22b803af40d00ec9c
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-08-14 18:44:31 -0300

    String de conexão.

--- src/conexao.php
+++ src/conexao.php
@@ -1,5 +1,7 @@
 <?php
 
+$caminhoBanco = __DIR__ . '/banco.sqlite'; 
+// A documentação recomenda que o caminho absoluto seja enviado para a connection string.
 // Estrutura padrão: $pdo = new PDO(connection_stsring, usuario, senha, array_de_parms_extra);
-$pdo = new PDO('sqlite:banco.sqlite');
+$pdo = new PDO('sqlite:' . $caminhoBanco);
 echo 'Conectei.';
