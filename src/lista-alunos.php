e867c24d172ee44aea5ba0fa2175e6e73b2bca6a
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-08-14 20:04:41 -0300

    Buscas específicas.

--- src/lista-alunos.php
+++ src/lista-alunos.php
@@ -7,11 +7,24 @@ require_once '../vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite'; 
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$statement = $pdo->query("SELECT * FROM students;");
-// $studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
-$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
-$studentList = [];
+$statement = $pdo->query("SELECT * FROM students WHERE id = 1;");
+/* Exemplo com fetchColumn (pega apenas a coluna da próxima linha).
+var_dump($statement->fetchColumn(1));
+exit();
+*/
+/* Exemplo com fetch (pega uma linha por vez)
+while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $student = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new DateTimeImmutable($studentData['birth_date'])
+    );
 
+    echo $student->age() . PHP_EOL;
+}
+exit();
+*/
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
 foreach($studentDataList as $studentData) {
     $studentList[] = new Student(
         $studentData['id'],
