9b7b1039c81efd1cbcd6b288f408c2c6a2a261d4
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-08-14 19:42:55 -0300

    Modos de busca.

--- src/lista-alunos.php
+++ src/lista-alunos.php
@@ -1,10 +1,23 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once '../vendor/autoload.php';
 
 $databasePath = __DIR__ . '/banco.sqlite'; 
 $pdo = new PDO('sqlite:' . $databasePath);
 
 $statement = $pdo->query("SELECT * FROM students;");
+// $studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
 
-var_dump($statement->fetchAll());
+foreach($studentDataList as $studentData) {
+    $studentList[] = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new DateTimeImmutable($studentData['birth_date'])
+    );
+}
+
+var_dump($studentList);
