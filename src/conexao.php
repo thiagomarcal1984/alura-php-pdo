b13a7affb42b0f2b2d575444ac216dbc70dd2c03
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-08-20 16:40:25 -0300

    Criando o telefone.

--- src/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$caminhoBanco = __DIR__ . '/banco.sqlite'; 
-// A documentação recomenda que o caminho absoluto seja enviado para a connection string.
-// Estrutura padrão: $pdo = new PDO(connection_stsring, usuario, senha, array_de_parms_extra);
-$pdo = new PDO('sqlite:' . $caminhoBanco);
-echo 'Conectei.';
-
-$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
